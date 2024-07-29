6745e68efe7a15def191fd66b733c41d2d7421cc
BlackDragonCat <1142537820@qq.com>
2024-07-29 10:57:55 +0800

    首页样式

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-import * as THREE from 'three';
+const router = document.getElementById("router")
+router.addEventListener('mouseenter',()=>{
+    //获取滚动条dom
+    router.style.overflowY = 'scroll'
+    console.log('1111');
+
+})
+router.addEventListener('mouseleave',()=>{
+    //获取滚动条dom
+    router.style.overflowY = 'hidden'
+
+})
+function toPage(path){
+    document.getElementById('dynamicIframe').src = '/src/components/'+ path + '/index.html';
+    
+}
